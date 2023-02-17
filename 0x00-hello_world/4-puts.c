f130a9830edccace997696242c679ce54435c74f
Tobestfol2 <tobestfol2@gmail.com>
2023-02-16 20:18:46 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  *  main - A programm that prints a line with puts function
  *  Return: 0
  */
-ini main(void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
