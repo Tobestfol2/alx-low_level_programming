a50afc301c203b623b089aab0b3c2f322cafa9a8
Tobestfol2 <tobestfol2@gmail.com>
2023-02-16 20:14:09 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main - A programm that prints a line with puts function
+ *  Return: 0
+ */
+init main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
