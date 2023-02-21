9be6aa3cda038913bd479c21bcf6ea0b2775cbc8
Tobestfol2 <tobestfol2@gmail.com>
2023-02-21 09:17:49 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main -prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: o
+ */
+
+
+void print_alphabet(void);
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+
+	_putchar('\n');
+}
