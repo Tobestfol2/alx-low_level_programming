b71ea120193dbdf31491d4d0d4b545b9ce16eb77
Tobestfol2 <tobestfol2@gmail.com>
2023-02-21 08:32:56 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_HEADER
+#define HOLBERTON_HEADER
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
