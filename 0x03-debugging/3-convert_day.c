008f6c9182d4b19e6755a616e4ef2ce1eca74483
Tobestfol2 <tobestfol2@gmail.com>
2023-02-24 13:03:22 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -10,7 +10,7 @@
 
 int convert_day(int month, int day)
 {
-	switch (month);
+	switch (month)
 	{
 	case 2:
 		day = 31 + day;
--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
 * main - takes a date and prints how many days are left in the year, taking
 * leap years into account
@@ -12,9 +13,9 @@ int main(void)
 	int day;
 	int year;
 
-	month = 4;
-	day = 01;
-	year = 1997;
+	month = 02;
+	day = 29;
+	year = 2000;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
