f1ffafb7123349a50ceff1f4d7f6a35030207862
Tobestfol2 <tobestfol2@gmail.com>
2023-03-02 05:07:37 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,7 +11,7 @@ char *leet(char *s)
 	int i, j;
 
 	char *a = "aAeEoOtTlL";
-	Char *b = "4433007711";
+	char *b = "4433007711";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -25,4 +25,3 @@ char *leet(char *s)
 	}
 	return (s);
 }
-
