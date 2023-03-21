3a12d466f61aaf69c035e27a739e07526cf3db54
Tobestfol2 <tobestfol2@gmail.com>
2023-03-20 19:07:15 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,21 +7,18 @@
  * @age: Second member
  * @owner: Third member
  *
- * Description: Longer desciption
+ * Description: Longer description
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-
 };
 
 /**
  * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
