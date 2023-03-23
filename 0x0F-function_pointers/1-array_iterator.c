7623e7333feb8c92da5258e0fb42c014b679d1a9
Tobestfol2 <tobestfol2@gmail.com>
2023-03-22 21:32:40 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (!array || !action)
+	if (array == NULL || action == NULL)
 		return;
 
 	for (i = 0; i < size; i++)
+	{
 		action(array[i]);
+	}
 }
