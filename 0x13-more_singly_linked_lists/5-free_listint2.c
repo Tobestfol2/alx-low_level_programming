7e953b4e70cc36268e77aee03b289189c2fb3065
Tobestfol2 <tobestfol2@gmail.com>
2023-04-04 19:38:10 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Freed a linked list
+ * @head: Pointer to the listint_t list to be freed
+ *
+ * Tobest_codes
+ *
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	*head = NULL;
+}
