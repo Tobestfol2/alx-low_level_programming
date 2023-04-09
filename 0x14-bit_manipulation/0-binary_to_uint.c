0830b5ba7d46b750435de72f57dc9a2cbd85312b
Tobestfol2 <tobestfol2@gmail.com>
2023-04-09 12:02:15 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,9 +18,9 @@ unsigned int binary_to_unit(const char *b)
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] < '0' || b[i] > 'i')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		binary = 2 * binary + (b[i] - "0");
+		binary = 2 * binary + (b[i] - '0');
 	}
 	return (binary);
 }
