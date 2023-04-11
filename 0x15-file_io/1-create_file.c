86d9162f75a53b61d53e4fb2f6ac7472c08a3680
Tobestfol2 <tobestfol2@gmail.com>
2023-04-11 01:09:45 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,30 +13,26 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int o = 0;
+	int fd;
+	int w;
+	int length = 10;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-
-	o = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
-	if (o == -1)
-	{
-		return (-1);
-	}
 
 	if (text_content != NULL)
 	{
-		size_t length = strlen(text_content);
-		ssize_t ret = write(o, text_content, length);
-
-		if (ret == -1)
-		{
-			close(o);
-
-			return (1);
-		}
+		for (length = 0; text_content[length];)
+			length++;
 	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, length);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
