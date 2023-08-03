244b9f5f4a853e871137df9d3f75294893197035
tobestfol2 <tobestfol2@gmail.com>
2023-08-03 07:42:04 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_bit -get the value of a bit at a given index
+ * @n: number to evaluate
+ * @index: index starting from 0, of the bit we want to get
+ *
+ * Tobest_codes
+ *
+ * Return: Value of bit at index, or -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit_size;
+
+	if (index > 63)
+		return (-1);
+	bit_size = (n >> index) & 1;
+
+	return (bit_size);
+}
