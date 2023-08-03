4b921589c4344694237bd0b8f85700e5bfafdd5c
tobestfol2 <tobestfol2@gmail.com>
2023-08-03 09:23:11 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * get_endianess - Chesks for endianness
+ *
+ * Tobest_codes
+ *
+ * Return: 0 for big endian, 1 for small endian
+ */
+
+int get_endianness(void)
+{
+	int num;
+
+	num = 1;
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
+}
