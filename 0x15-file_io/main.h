b39a7a2ca71e645fbc184d737b62296e67e9d416
tobestfol2 <tobestfol2@gmail.com>
2023-08-07 17:53:22 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: 0x15. C - File I/O
+ * Auth: Tobest_codes
+ * Desc: Header file of prototypes for all functions
+ * written in the 0x15-file_i/o directory.
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /*MAIN_H*/
