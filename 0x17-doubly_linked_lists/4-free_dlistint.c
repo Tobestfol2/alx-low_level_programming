8b723695a3831169ad5711c1738402bd0d772946
tobestfol2 <tobestfol2@gmail.com>
2023-08-17 10:58:40 -0700

    4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: pointer to head of the list
+ *
+ * Tobest_codes
+ *
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+
+	return;
+
+	while (head->next)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+	free(head);
+}
