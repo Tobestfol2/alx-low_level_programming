a209b293e1b08216afbe1a08f20d0267f9661ca9
tobestfol2 <tobestfol2@gmail.com>
2023-08-17 10:09:14 -0700

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,32 +1,32 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints the elements of
- * dlistint_t list
+ * dlistint_len - retruns the number of elements in
+ * double linked list
  *
- * @: head of list
+ * @n: head of the list
  *
  * Tobest_codes
  *
- * Return: the number of nodes
+ * Returns: the number of nodes
  */
-
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
-	
+
 	count = 0;
-	
+
 	if (h == NULL)
 		return (count);
+
 	while (h->prev != NULL)
 		h = h->prev;
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
+
 	return (count);
 }
