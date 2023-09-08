4cd8c88f283ed20287a7465ccad56708ea9709a2
tobestfol2 <tobestfol2@gmail.com>
2023-09-08 09:08:51 -0700

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,6 +11,7 @@
  * Return: Returns an integer
  * Tobest_codes
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int hash;
@@ -19,30 +20,38 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (strlen(key) == 0 || strlen(value) == 0)
 		return (0);
-
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
-
 	node->key = malloc(strlen(key) + 1);
 	node->value = malloc(strlen(value) + 1);
-
 	if (node->key == NULL || node->value == NULL)
 	{
 		free(node);
 		return (0);
 	}
-
 	node->next = NULL;
 	strcpy(node->key, key);
 	strcpy(node->value, value);
-	hash = hash_djb2(key);
+	hash =  hash_djb2(key);
+	printf("Working here\n");
+
+	/**
+	 * if (strcmp(ht->array[hash]->key, node->key) == 0)
+	{
+		ht->array[hash]->value = node->value;
+	}
+	*/
 
 	if (ht->array[hash] == NULL)
-
 	{
 		ht->array[hash] = node;
 	}
-
+	else
+	{
+		temp = ht->array[hash];
+		temp->next = NULL;
+		node->next = temp;
+	}
 	return (1);
 }
