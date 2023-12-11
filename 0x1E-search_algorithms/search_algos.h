02f25dc39469866bf5cb7154972214f93fc56318
tobestfol2 <tobestfol2@gmail.com>
2023-12-11 14:01:02 -0800

    0. Linear search

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+#include <stddef.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+#endif
